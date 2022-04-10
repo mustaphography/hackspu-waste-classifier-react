78649e9123416897baa26bd86cbfe61e86950cc7
raymond-design <raymondyan777@gmail.com>
2022-04-10 06:31:14 -0400

    processor

---
+++ src/services/processor.js
@@ -0,0 +1,12 @@
+import { Axios } from 'axios';
+
+//returns formatted ai analysis data
+export default function processor(image) {
+  //get image object from camera
+
+  //turn object into correct format
+
+  //request to ai model
+
+  //return and format result
+}
