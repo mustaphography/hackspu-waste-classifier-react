2a94703f3d4f0454c068acc5622cd8bc82934a4a
raymond-design <raymondyan777@gmail.com>
2022-04-10 03:53:35 -0400

    cleaned project

---
+++ src/components/Camera/camera.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { useState, useEffect } from 'react';
+// import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+
+export default function CameraComponent() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  const styles = StyleSheet.create({  
+    container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    },
+    camera: {
+    height:800,
+    width:450,
+    },
+  }); 
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type}>
+        <View >
+          <TouchableOpacity
+            
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text > Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
+  );
+}
